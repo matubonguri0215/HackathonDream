793c6237f9b92a0b897a13ddb964a07b5e343ca8
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2026-01-17 14:10:04 +0900

    銃更新

--- Assets/Scripts/Entities/AssaultRifle.cs
+++ Assets/Scripts/Entities/AssaultRifle.cs
@@ -4,7 +4,7 @@ public class AssaultRifle : WeaponBase
 {
     private protected override void DoShot(Vector2 shotDirection, float bulletDamage)
     {
-        _bulletBase = Instantiate(_bulletBase, _InitBulletPos.position, Quaternion.identity);
-        _bulletBase.Initialize(shotDirection, _weaponDamage);
+        WeaponBulletBase bullet = Instantiate(_bulletBase, _InitBulletPos.position, Quaternion.identity);
+        bullet.Initialize(shotDirection, bulletDamage, moveSpeed: _moveSpeed);
     }
 }
